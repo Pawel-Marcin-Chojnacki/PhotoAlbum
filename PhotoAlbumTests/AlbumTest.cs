60f90ea2375ed9556632b31487b843378e0d65cc
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2016-03-01 13:49:13 +0100

    + Randomized album names for providing more thread safety. + Independence of the location on disk. + Performance improvement. + Refactor that lead to more readable code.

--- PhotoAlbumTests/AlbumTest.cs
+++ PhotoAlbumTests/AlbumTest.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
+using System.Reflection;
 using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PhotoAlbum;
@@ -14,12 +17,14 @@ namespace PhotoAlbumTests
     [TestClass]
     public class AlbumTest
     {
+
+        RandomObjects random = new RandomObjects();
+
         [TestMethod]
         public void CreateAlbumTest()
         {
-
             // arrange 
-            var name = "Clocks";
+            var name = random.GetRandomName();
             Uri location = new Uri(Directory.GetCurrentDirectory() + name);
 
             // act
@@ -32,140 +37,75 @@ namespace PhotoAlbumTests
         [TestMethod]
         public void CreateEmptyAlbumTest()
         {
-            //arrange
-            // act
             var album = new Album();
-            //Thread.Sleep(1000);
-
-            //assert
             Assert.AreEqual(DateTime.Now,album.CreationDate);
         }
 
         [TestMethod]
         public void AddPhotoToAlbumTest()
         {
-            var name = "Clocks";
+            var name = random.GetRandomName();
+            
             Uri location = new Uri(Directory.GetCurrentDirectory() + name);
-            var photo = new Photo();
-            var photoLocation = new Uri(location.AbsolutePath + "/Untitled.jpg");
 
             // act
             var album = new Album(location, name, DateTime.Now);
-            photo.Open(photoLocation);
-            album.Photos.Add(photo);
+            album.Photos.Add(random.CreatePhoto(location.AbsolutePath));
             
             //Assert
             Assert.AreEqual(1, album.Photos.Count);
-            album.Photos[0].Delete();
-
-            album.Photos.Remove(photo);
-            Assert.AreEqual(0, album.Photos.Count);
         }
 
         [TestMethod]
         public void AddTagsFromPhotosInAlbumTest()
         {
-            var name = "Clocks";
+            var name = random.GetRandomName();
+            var tagsPerPicture = 4;
             Uri location = new Uri(Directory.GetCurrentDirectory() + "/" + name);
             var album = new Album(location, name, DateTime.Now);
-
-            File.Copy(Directory.GetCurrentDirectory() + "/Untitled.jpg", Directory.GetCurrentDirectory() + "/" + name + "/Untitled.jpg", true);
-            File.Copy(Directory.GetCurrentDirectory() + "/Untitled.jpg", Directory.GetCurrentDirectory() + "/" + name + "/Untitled2.jpg", true);
-            var photo = new Photo();
-            var photoLocation = new Uri(location.AbsolutePath + "/Untitled.jpg");
-            var photo2 = new Photo();
-            var photoLocation2 = new Uri(location.AbsolutePath + "/Untitled2.jpg");
-            photo.Open(photoLocation);
-            album.Photos.Add(photo);
-            photo2.Open(photoLocation2);
-            album.Photos.Add(photo2);
-
-            List<string> tags = new List<string>()
-            {
-                "outside",
-                "manual",
-                "morning"
-            };
-            var addTagsResult = album.Photos[0].AddTags(tags);
-            List<string> tags2 = new List<string>()
-            {
-                "plan",
-                "schedule",
-                "university"
-            };
-            var addTagsResult2 = album.Photos[1].AddTags(tags2);
-
-
-            //act
+            
+            album.Photos.Add(random.CreatePhoto(location.AbsolutePath));
+            album.Photos.Add(random.CreatePhoto(location.AbsolutePath));
+            foreach (var photo in album.Photos)
+                photo.AddTags(random.GenerateTags(tagsPerPicture));
             album.AddTagsFromPhotosInAlbum();
 
-            // assert
-            Assert.AreEqual("work;clock;outside;manual;morning;plan;schedule;university", String.Join(";",Tag.Tags));
-
+            Assert.AreEqual(tagsPerPicture * album.Photos.Count, album._tags.Count);
         }
 
+        
 
         [TestMethod]
         public void SetTagsFromPhotosInAlbumTest()
         {
-            var name = "Lights";
+            var name = random.GetRandomName();
             Uri location = new Uri(Directory.GetCurrentDirectory() + "/" + name);
             var album = new Album(location, name, DateTime.Now);
-
-            File.Copy(Directory.GetCurrentDirectory() + "/Untitled.jpg", Directory.GetCurrentDirectory() + "/" + name + "/Untitled.jpg", true);
-            File.Copy(Directory.GetCurrentDirectory() + "/Untitled.jpg", Directory.GetCurrentDirectory() + "/" + name + "/Untitled2.jpg", true);
-            var photo = new Photo();
-            var photoLocation = new Uri(location.AbsolutePath + "/Untitled.jpg");
-            var photo2 = new Photo();
-            var photoLocation2 = new Uri(location.AbsolutePath + "/Untitled2.jpg");
-            photo.Open(photoLocation);
-            album.Photos.Add(photo);
-            photo2.Open(photoLocation2);
-            album.Photos.Add(photo2);
-
-            List<string> tags = new List<string>()
-            {
-                "outside",
-                "manual",
-                "morning"
-            };
-            var addTagsResult = album.Photos[0].SetTags(tags);
-            List<string> tags2 = new List<string>()
-            {
-                "plan",
-                "schedule",
-                "university"
-            };
-            var addTagsResult2 = album.Photos[1].SetTags(tags2);
-
+            var tagsPerPicture = 4;
+            album.Photos.Add(random.CreatePhoto(location.AbsolutePath));
+            album.Photos.Add(random.CreatePhoto(location.AbsolutePath));
+            foreach (var photo in album.Photos)
+                photo.AddTags(random.GenerateTags(tagsPerPicture));
             //act
             album.AddTagsFromPhotosInAlbum();
 
             // assert
-            Assert.AreEqual("outside;manual;morning;plan;schedule;university", String.Join(";", Tag.Tags));
+            Assert.AreEqual(tagsPerPicture*album.Photos.Count, album._tags.Count);
 
         }
 
         [TestMethod]
         public void DeleteAlbumTest()
         {
-            var name = "Street";
+            var name = random.GetRandomName();
             Uri location = new Uri(Directory.GetCurrentDirectory() + "/" + name);
             var album = new Album(location, name, DateTime.Now);
-            File.Copy(Directory.GetCurrentDirectory() + "/Untitled.jpg", Directory.GetCurrentDirectory() + "/" + name + "/Untitled.jpg", true);
-            File.Copy(Directory.GetCurrentDirectory() + "/Untitled.jpg", Directory.GetCurrentDirectory() + "/" + name + "/Untitled2.jpg", true);
-            var photo = new Photo();
-            var photoLocation = new Uri(location.AbsolutePath + "/Untitled.jpg");
-            var photo2 = new Photo();
-            var photoLocation2 = new Uri(location.AbsolutePath + "/Untitled2.jpg");
-            photo.Open(photoLocation);
-            album.Photos.Add(photo);
-            photo2.Open(photoLocation2);
-            album.Photos.Add(photo2);
+            album.Photos.Add(random.CreatePhoto(location.AbsolutePath));
+            album.Photos.Add(random.CreatePhoto(location.AbsolutePath));
 
             album.Delete();
-            
 
+            Assert.AreEqual(false,Directory.Exists(location.AbsolutePath));
         }
 
     }
