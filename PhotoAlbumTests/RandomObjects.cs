a46c55e2290526087e24eb7a225a1e26a481acc6
Paweł Marcin Chojnacki <pawel.marcin.chojnacki@gmail.com>
2016-03-01 13:49:58 +0100

    Class for generating random names, filenames, tags for testing.

---
+++ PhotoAlbumTests/RandomObjects.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhotoAlbum;
+
+namespace PhotoAlbumTests
+{
+    class RandomObjects
+    {
+        private Random rnd = new Random();
+
+        public List<string> GenerateTags(int numberOfTags)
+        {
+            var list = new List<string>();
+            for (int i = 0; i < numberOfTags; i++)
+            {
+                list.Add(Path.GetRandomFileName().Replace(".",""));
+            }
+            return list;
+        }
+
+        public Photo CreatePhoto(string absolutePath)
+        {
+            var randomPhotoName = "/" + rnd.Next().ToString() + ".jpg";
+            new Bitmap(1, 1).Save(absolutePath + randomPhotoName, ImageFormat.Jpeg);
+            var photo = new Photo();
+            photo.Open(new Uri(absolutePath + randomPhotoName));
+            return photo;
+        }
+
+        public string GetRandomName()
+        {
+            return Path.GetRandomFileName().Replace(".", "");
+        }
+    }
+}
